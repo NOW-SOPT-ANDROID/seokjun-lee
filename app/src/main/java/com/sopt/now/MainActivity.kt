28fc9a343caccd85e808300c645488c11b069de1
boiledegg <tjrwns2163@naver.com>
2024-04-03 00:38:32 +0900

    feat: 메인 페이지 UI 구현

--- app/src/main/java/com/sopt/now/MainActivity.kt
+++ app/src/main/java/com/sopt/now/MainActivity.kt
@@ -5,11 +5,14 @@ import androidx.activity.enableEdgeToEdge
 import androidx.appcompat.app.AppCompatActivity
 import androidx.core.view.ViewCompat
 import androidx.core.view.WindowInsetsCompat
+import com.sopt.now.databinding.ActivityMainBinding
 
 class MainActivity : AppCompatActivity() {
+    private lateinit var binding: ActivityMainBinding
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_main)
+        binding = ActivityMainBinding.inflate(layoutInflater)
+        setContentView(binding.root)
 
     }
 }
