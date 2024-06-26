02cb1aeb40fbc484b73d63a61e6c72e12f5e8ebe
boiledegg <tjrwns2163@naver.com>
2024-06-26 15:04:31 +0900

    CHORE/#21: 로컬데이터베이스 모듈 수정

--- app/src/main/java/com/sopt/now/compose/di/SharedPreferenceModule.kt
+++ app/src/main/java/com/sopt/now/compose/di/SharedPreferenceModule.kt
@@ -3,13 +3,18 @@ package com.sopt.now.compose.di
 import android.content.Context
 import android.content.SharedPreferences
 import dagger.Module
+import dagger.Provides
 import dagger.hilt.InstallIn
 import dagger.hilt.android.qualifiers.ApplicationContext
 import dagger.hilt.components.SingletonComponent
+import javax.inject.Singleton
 
 @Module
 @InstallIn(SingletonComponent::class)
 object SharedPreferenceModule {
+
+    @Provides
+    @Singleton
     fun provideSharedPreference(@ApplicationContext context: Context): SharedPreferences =
         context.getSharedPreferences(context.packageName, Context.MODE_PRIVATE)
 }
